251a7e51fe3916e1c44e0e483824501df73c0051
luciatirta <luciatirtag@gmail.com>
2021-01-10 19:59:58 +0800

    Style signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import { signup, signUpWithGoogle } from '../../services/auth';
 import './styles.css'
@@ -45,60 +45,82 @@ class SignUp extends Component {
 
   render() {
     return (
-      <div className="ui container" id="signup">
-        <div className="ui form">
-          <div className="field">
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Full Name"
-              onChange={this.handleChange}
-              value={this.state.name}
-              required
-            />
+      <Fragment>
+        <div className="ui stackable grid" id="signup">
+          <div className="eight wide column" id="left-signup">
+            <div id="innerleft-signup">
+              <h1>Learning for students.</h1>
+              <h1>Learning for teachers.</h1>
+              <img
+                src={require('../../data/img/people.svg')}
+                className="image-people"
+              />
+            </div>
           </div>
-          <div className="field">
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email address"
-              onChange={this.handleChange}
-              value={this.state.email}
-              required
-            />
-          </div>
-          <div className="field">
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              onChange={this.handleChange}
-              value={this.state.password}
-              required
-              pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}"
-              title="Password must be a combination of number and letters, and at least 8 or more characters"
-            />
-          </div>
-          <div className="field">
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              name="password2"
-              placeholder="Confirm Password"
-              onChange={this.handleChange}
-              value={this.state.password2}
-              required
-              pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}"
-              title="Password must be a combination of number and letters, and at least 8 or more characters"
-            />
+          <div className="eight wide column" id="right-signup">
+            <div id="innerright-signup">
+              <h1>Sign Up </h1>
+              <div className="ui form">
+                <div className="field">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Full Name"
+                    onChange={this.handleChange}
+                    value={this.state.name}
+                    required
+                  />
+                </div>
+                <div className="field">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email address"
+                    onChange={this.handleChange}
+                    value={this.state.email}
+                    required
+                  />
+                </div>
+                <div className="field">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                    required
+                    pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}"
+                    title="Password must be a combination of number and letters, and at least 8 or more characters"
+                  />
+                </div>
+                <div className="field">
+                  <label>Confirm Password</label>
+                  <input
+                    type="password"
+                    name="password2"
+                    placeholder="Confirm Password"
+                    onChange={this.handleChange}
+                    value={this.state.password2}
+                    required
+                    pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}"
+                    title="Password must be a combination of number and letters, and at least 8 or more characters"
+                  />
+                </div>
+                <button className="ui fluid button" id="button-signup" onClick={this.handleSubmit}>Sign Up</button>
+                <div className="ui horizontal divider">OR</div>
+                <button className="ui basic fluid button" id="button-signupGoogle" onClick={this.googleSignIn}>Register with Google</button>
+              </div>
+              <img
+                src={require('../../data/img/Made_with_love.svg')}
+                className="image-madewlove"
+              />
+            </div>
           </div>
         </div>
-        <button className="ui button" onClick={this.handleSubmit}>Sign Up</button>
-        <button className="ui button" onClick={this.googleSignIn}>Sign Up with Google</button>
-      </div>
+      </Fragment>
     );
   }
 }
